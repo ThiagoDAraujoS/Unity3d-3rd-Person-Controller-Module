3460245da3a96c88ab0a444ff382ccc9739de2e0
Thiago <ThiagoDAraujoS@Gmail.com>
2019-07-05 06:51:48 -0700

    0.0.1
    Added Base Movement and Base Movement Editor
    Added Base Grounding Mechanics (need testing)
    Added Base Move and Turn mechanics (need testing)

---
+++ Base Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour
+{
+    [SerializeField][Range(0f,10f)]
+    protected float moveSpeed, rotateSpeed, jumpForce;
+    [SerializeField][Range(0f,90f)]
+    protected float climbAngleLimit;
+
+    [SerializeField]
+    protected AnimationCurve rotationAccelerationCurve, movementAccelerationCurve;
+    [SerializeField]
+    public Rigidbody rb;
+    private bool isGrounded = false;
+    //[HideInInspector]
+    public float feetHeight= -0.5f;
+    public bool IsGrouded {
+        get { return isGrounded; }
+    }
+
+    public virtual void Jump(){
+        if(isGrounded){
+            Vector3 newVelocity = rb.velocity;
+            newVelocity.y = jumpForce;
+            rb.velocity = newVelocity;
+        }
+    }
+    void Start(){
+        climbAngleLimit/=90f;
+        StartCoroutine(UpdateIsGroundedFlag());
+    }
+    public virtual void Walk(Vector2 direction){
+        rb.AddTorque(transform.up * rotateSpeed * rotationAccelerationCurve.Evaluate(direction.y) * direction.x, ForceMode.VelocityChange);
+        rb.AddForce(transform.forward * moveSpeed * movementAccelerationCurve.Evaluate(direction.x), ForceMode.Acceleration);
+    }
+
+    private bool tempIsGrounded = false;
+    private IEnumerator UpdateIsGroundedFlag(){
+        //run forever
+        while(true){
+            //wait until the last call on the physics pipeline
+
+            //[DANGER]if i need to use the "isgounded
+            yield return new WaitForFixedUpdate();
+
+            //move the tempValue of isgrounded into the main isgrounded
+            //at the start of the next frame the new is grounded will be taken as the real flag
+            isGrounded = tempIsGrounded;
+
+            //set the fake is grounded as false
+            tempIsGrounded = false;
+
+            //if the collision never happens the is grounded is set to false releasing the character so he can jump
+            //since this happens on the very end of the step, the IS grounded always take 1 frame to trully happen
+            //but since its the LAST thing there will be no weirdness when i call it on the fixed update
+        }
+    }
+
+    private void OnCollisionStay(Collision collisionInfo) {
+        //For each collision point
+        for (int i = 0; i < collisionInfo.contactCount && !isGrounded; i++)
+            //check if the point's normal is looking up and 
+            tempIsGrounded = ((Vector3.Dot(collisionInfo.contacts[i].normal, Vector3.up)>= climbAngleLimit)  &&  collisionInfo.contacts[i].point.y <= transform.position.y + feetHeight);
+    }
+}
---
+++ Editor/Base Movement Editor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof (BaseMovement))]
+public class BaseMovementEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    void OnSceneGUI(){
+        BaseMovement o = target as BaseMovement;
+
+        if(o == null || o.rb == null)
+            return;
+        
+        Handles.color = Color.blue;
+        Vector3 pos = Vector3.up * o.feetHeight + o.rb.position;
+        //Vector3 pos = Vector3.zero + o.transform.position;
+        o.feetHeight = Handles.ScaleValueHandle(o.feetHeight, pos, Quaternion.Euler(90f, 0f, 0f),3.5f,Handles.RectangleHandleCap,0.1f);
+    }
+}
